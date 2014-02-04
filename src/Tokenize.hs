71a74432339e30ea288faad9fa1584151643c257
Paul Phillips <PaulPhillips@aggienetwork.com>
2014-02-03 20:23:56 -0600

    Use a guard statement to make the pattern matching a little easier to read.

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -1,11 +1,15 @@
 module Tokenize where
 
+import Data.List
+
 data Token = Function | OpenParens | CloseParens | OpenBrace | CloseBrace deriving (Eq, Show)
 
 tokenize :: String -> [Token]
-tokenize ('f':'u':'n':'c':'t':'i':'o':'n':xs) = [Function] ++ (tokenize xs)
 tokenize ('(':xs) = [OpenParens] ++ tokenize xs
 tokenize (')':xs) = [CloseParens] ++ tokenize xs
 tokenize ('{':xs) = [OpenBrace] ++ tokenize xs
 tokenize ('}':xs) = [CloseBrace] ++ tokenize xs
+tokenize stream  
+	| "function" `isPrefixOf` stream = [Function] ++ (tokenize remainder)
+	where remainder = drop (length "function") stream
 tokenize _ = []
