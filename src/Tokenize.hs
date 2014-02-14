798bf8c39cd4040e4d4924695b13c4879c56493a
Paul Phillips <PaulPhillips@aggienetwork.com>
2014-02-14 16:03:52 -0600

    Renamed variables to shorten lines.

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -8,18 +8,16 @@ tokenize :: String -> [Token]
 tokenize [] = []
 tokenize (' ':xs) = tokenize xs
 tokenize ('\t':xs) = tokenize xs
-tokenize stream = removeEmptyIdentifiers $ tokenize' (Unfinished "") stream
-
-removeEmptyIdentifiers :: [Token] -> [Token]
-removeEmptyIdentifiers ts = filter (\x -> not $ isEmptyIdentifier x) ts
+tokenize stream = tokenize' (Unfinished "") stream
 
 tokenize' :: PartialToken -> String -> [Token]
-tokenize' (Finished token) stream = [token] ++ tokenize stream 
+tokenize' (Finished t) xs = [t] ++ tokenize xs 
 tokenize' unfinished (x:xs) = lookahead unfinished xs $ accumulate [x]
 
 lookahead :: PartialToken -> String -> PartialToken -> [Token]
-lookahead (Unfinished string) xs (Finished token) = [Identifier string, token] ++ tokenize xs
-lookahead (Unfinished string) xs (Unfinished x) = tokenize' (accumulate (string ++ x)) xs
+lookahead (Unfinished "") xs (Finished t) = [t] ++ tokenize xs
+lookahead (Unfinished s) xs (Finished t) = [Identifier s, t] ++ tokenize xs
+lookahead (Unfinished s) xs (Unfinished x) = tokenize' (accumulate (s ++ x)) xs
 
 data PartialToken = Finished Token | Unfinished String | Empty
 
@@ -29,7 +27,3 @@ accumulate x = fromMaybe (toToken x) x
 fromMaybe :: Maybe Token -> String -> PartialToken
 fromMaybe (Just t) _ = Finished t
 fromMaybe Nothing s  = Unfinished s
-
-isEmptyIdentifier :: Token -> Bool
-isEmptyIdentifier (Identifier "") = True
-isEmptyIdentifier _ = False
