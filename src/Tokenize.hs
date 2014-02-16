e95212f35ead28150503109c71813903ff48c756
Paul Phillips <PaulPhillips@aggienetwork.com>
2014-02-16 03:33:01 -0600

    Simplified types a little bit.

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -8,28 +8,28 @@ tokenize :: String -> [Token]
 tokenize []        = []
 tokenize (' ':xs)  = tokenize xs
 tokenize ('\t':xs) = tokenize xs
-tokenize ('\"':xs) = tokenizeLiteralString (Unfinished "") xs
+tokenize ('\"':xs) = tokenizeLiteralString (Current "") xs
 tokenize stream    = tokenize' (Unfinished "") stream
 
 tokenize' :: PartialToken -> String -> [Token]
 tokenize' (Finished t) xs   = [t] ++ tokenize xs 
-tokenize' unfinished (x:xs) = lookahead (Current unfinished) xs $ Next (accumulate [x])
+tokenize' (Unfinished s) (x:xs) = lookahead (Current s) xs $ Next (accumulate [x])
 tokenize' (Unfinished s) [] = [Identifier s]
 
 lookahead :: Current -> String -> Next -> [Token]
-lookahead (Current (Unfinished "")) xs (Next (Finished t))   = [t] ++ tokenize xs
-lookahead (Current (Unfinished s) ) xs (Next (Finished t))   = [Identifier s, t] ++ tokenize xs
-lookahead (Current (Unfinished s) ) xs (Next (Unfinished x)) = tokenize' (accumulate (s ++ x)) xs
+lookahead (Current "") xs (Next (Finished t))   = [t] ++ tokenize xs
+lookahead (Current s)  xs (Next (Finished t))   = [Identifier s, t] ++ tokenize xs
+lookahead (Current s)  xs (Next (Unfinished x)) = tokenize' (accumulate (s ++ x)) xs
 
-tokenizeLiteralString :: PartialToken -> String -> [Token]
-tokenizeLiteralString (Unfinished s) ('\"':xs) = [quote s] ++ tokenize xs
-tokenizeLiteralString (Unfinished s) (x:xs)    = tokenizeLiteralString (Unfinished (s ++ [x])) xs
+tokenizeLiteralString :: Current -> String -> [Token]
+tokenizeLiteralString (Current s) ('\"':xs) = [quote s] ++ tokenize xs
+tokenizeLiteralString (Current s) (x:xs)    = tokenizeLiteralString (Current $ s ++ [x]) xs
 
 quote :: String -> Token
 quote s = Identifier $ "\"" ++ s ++ "\""
 
 data PartialToken = Finished Token | Unfinished String
-data Current = Current PartialToken
+data Current = Current String
 data Next = Next PartialToken
 
 accumulate :: String -> PartialToken
