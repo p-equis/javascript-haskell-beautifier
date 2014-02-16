cacafa1c42fced62163e9c19ab549c1157c05a41
Paul Phillips <PaulPhillips@aggienetwork.com>
2014-02-16 01:49:50 -0600

    Tried to prettify the code in Tokenize

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -5,20 +5,20 @@ module Tokenize (
 import Token
 
 tokenize :: String -> [Token]
-tokenize [] = []
-tokenize (' ':xs) = tokenize xs
+tokenize []        = []
+tokenize (' ':xs)  = tokenize xs
 tokenize ('\t':xs) = tokenize xs
-tokenize stream = tokenize' (Unfinished "") stream
+tokenize stream    = tokenize' (Unfinished "") stream
 
 tokenize' :: PartialToken -> String -> [Token]
-tokenize' (Finished t) xs = [t] ++ tokenize xs 
+tokenize' (Finished t) xs   = [t] ++ tokenize xs 
 tokenize' unfinished (x:xs) = lookahead (Current unfinished) xs $ Next (accumulate [x])
 tokenize' (Unfinished s) [] = [Identifier s]
 
 lookahead :: Current -> String -> Next -> [Token]
-lookahead (Current (Unfinished "")) xs (Next (Finished t)) = [t] ++ tokenize xs
-lookahead (Current (Unfinished s)) xs (Next (Finished t)) = [Identifier s, t] ++ tokenize xs
-lookahead (Current (Unfinished s)) xs (Next (Unfinished x)) = tokenize' (accumulate (s ++ x)) xs
+lookahead (Current (Unfinished "")) xs (Next (Finished t))   = [t] ++ tokenize xs
+lookahead (Current (Unfinished s) ) xs (Next (Finished t))   = [Identifier s, t] ++ tokenize xs
+lookahead (Current (Unfinished s) ) xs (Next (Unfinished x)) = tokenize' (accumulate (s ++ x)) xs
 
 data PartialToken = Finished Token | Unfinished String
 data Current = Current PartialToken
