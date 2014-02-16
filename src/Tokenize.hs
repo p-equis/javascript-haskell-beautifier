fc28314281380ce38f9920ff33a2a4d27c1884ae
Paul Phillips <PaulPhillips@aggienetwork.com>
2014-02-16 01:39:22 -0600

    Create some data types to make the signature of lookahead more understandable, hopefully.

--- src/Tokenize.hs
+++ src/Tokenize.hs
@@ -12,15 +12,17 @@ tokenize stream = tokenize' (Unfinished "") stream
 
 tokenize' :: PartialToken -> String -> [Token]
 tokenize' (Finished t) xs = [t] ++ tokenize xs 
-tokenize' unfinished (x:xs) = lookahead unfinished xs $ accumulate [x]
+tokenize' unfinished (x:xs) = lookahead (Current unfinished) xs $ Next (accumulate [x])
 tokenize' (Unfinished s) [] = [Identifier s]
 
-lookahead :: PartialToken -> String -> PartialToken -> [Token]
-lookahead (Unfinished "") xs (Finished t) = [t] ++ tokenize xs
-lookahead (Unfinished s) xs (Finished t) = [Identifier s, t] ++ tokenize xs
-lookahead (Unfinished s) xs (Unfinished x) = tokenize' (accumulate (s ++ x)) xs
+lookahead :: Current -> String -> Next -> [Token]
+lookahead (Current (Unfinished "")) xs (Next (Finished t)) = [t] ++ tokenize xs
+lookahead (Current (Unfinished s)) xs (Next (Finished t)) = [Identifier s, t] ++ tokenize xs
+lookahead (Current (Unfinished s)) xs (Next (Unfinished x)) = tokenize' (accumulate (s ++ x)) xs
 
 data PartialToken = Finished Token | Unfinished String
+data Current = Current PartialToken
+data Next = Next PartialToken
 
 accumulate :: String -> PartialToken
 accumulate x = fromMaybe (toToken x) x
